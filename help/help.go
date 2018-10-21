9b963047d89459a3e53b2e97ce7db618fbd71236
ehsun7b <ehsun7b@gmail.com>
2018-10-21 23:17:01 +0200

    help content

--- help/help.go
+++ help/help.go
@@ -2,7 +2,7 @@ package help
 
 func HowToRun() string {
 	content := `How to run
-	simple-chatroom-in-GoLang-RPC --mode server/client --port port_number --server-port [server_port_number]
+	simple-chatroom-in-GoLang-RPC --mode [server/client] --port [port_number] --server-port [server_port_number] --username [username] 
 for example:
 	simple-chatroom-in-GoLang-RPC --mode server --port 8080
 	simple-chatroom-in-GoLang-RPC --mode client --port 8081 --server-ip 127.0.0.1 --server-port 8080 --username John
